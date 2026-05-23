bfe3f75e519e089d0fd1d87842b69b5ce2adc3a8
Nanda Alfarizi <muhammadnandaalfarizi@gmail.com>
2026-05-23 16:37:49 +0700

    chore: add vercel deployment config

--- src/index.ts
+++ src/index.ts
@@ -33,6 +33,4 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+export default app;
